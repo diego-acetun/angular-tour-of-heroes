20d8ba3e3f1ab841c6f938d306cbef012a7d667f
diego-acetun <diego.acetun@pentcloud.com>
2022-11-17 18:39:25 -0600

    aplicando estilos tailwind

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -21,6 +21,14 @@ export class HeroService {
   ) {}
 
   /** GET heroes from the server */
+  // getHeroes(): Observable<Hero[]> {
+  //   return this.http.get<Hero[]>(this.heroesUrl).pipe(
+  //     tap((_) => this.log('fetched heroes')),
+  //     catchError(this.handleError<Hero[]>('getHeroes', []))
+  //   );
+  // }
+
+  
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl).pipe(
       tap((_) => this.log('fetched heroes')),
